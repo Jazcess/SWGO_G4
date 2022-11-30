f04a69728acc5df724c853f8700b77e67c1ea398
Jazmin S. Stewart <jss55@spectre11.cm.cluster>
2022-11-30 10:57:24 +0000

    Finished commenting construction.cc code

--- construction.cc
+++ construction.cc
@@ -26,37 +26,38 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
 	Aerogel->AddMaterial(H2O, 37.4*perCent);  //adding the h20 material to aergoel class
 	Aerogel->AddElement(C, 0.1*perCent);  //adding the carbon material to aerogel class
 
-	G4double energy[2] = {1.239841939*eV/0.9, 1.239841939*eV/0.2};
-	G4double rindexAerogel[2] = {1.1, 1.1};
-	G4double rindexWorld[2] = {1.0, 1.0};
+	G4double energy[2] = {1.239841939*eV/0.9, 1.239841939*eV/0.2}; //momentum of photons usng conversion factor *eV / wavlength of red light and blue light
 
-	G4MaterialPropertiesTable *mptAerogel = new G4MaterialPropertiesTable();
-	mptAerogel->AddProperty("RINDEX", energy, rindexAerogel, 2);
+	G4double rindexAerogel[2] = {1.1, 1.1}; //refractive index of aerogel
+	G4double rindexWorld[2] = {1.0, 1.0}; //refractive index of the world volume (air)
 
-	Aerogel->SetMaterialPropertiesTable(mptAerogel);
+	G4MaterialPropertiesTable *mptAerogel = new G4MaterialPropertiesTable(); //creating aerogel material properties class
+	mptAerogel->AddProperty("RINDEX", energy, rindexAerogel, 2); //adding property to this class
+
+	Aerogel->SetMaterialPropertiesTable(mptAerogel); //assigning the Aergoel class the properties from the previous class created
 	
-	G4Material *worldMat = nist->FindOrBuildMaterial("G4_AIR");
+	G4Material *worldMat = nist->FindOrBuildMaterial("G4_AIR"); //createing the world material (air)
 
-	G4MaterialPropertiesTable *mptWorld = new G4MaterialPropertiesTable();
-	mptWorld->AddProperty("RINDEX", energy, rindexWorld, 2);
+	G4MaterialPropertiesTable *mptWorld = new G4MaterialPropertiesTable(); //creating properites class for air
+	mptWorld->AddProperty("RINDEX", energy, rindexWorld, 2); //adding property to class
 
-	worldMat->SetMaterialPropertiesTable(mptWorld);
+	worldMat->SetMaterialPropertiesTable(mptWorld); //assigning properties class to world material class
 
-	G4Box *solidWorld = new G4Box("solidWorld", 0.5*m, 0.5*m, 0.5*m);
+	G4Box *solidWorld = new G4Box("solidWorld", 0.5*m, 0.5*m, 0.5*m); //setting up world volume as a box
 
-	G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
+	G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld"); //creating the world as dimensions from box and material (Air)
 
-	G4VPhysicalVolume *physWorld = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), logicWorld, "physWorld", 0, false, 0, true);
+	G4VPhysicalVolume *physWorld = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), logicWorld, "physWorld", 0, false, 0, true); //placing the world into geant 4 sim
 
-	G4Box *solidRadiator = new G4Box("solidRadiator", 0.4*m, 0.4*m, 0.01*m);
+	G4Box *solidRadiator = new G4Box("solidRadiator", 0.4*m, 0.4*m, 0.01*m); //setting the dimensions for the radiator
 
-	G4LogicalVolume *logicRadiator = new G4LogicalVolume(solidRadiator, Aerogel, "logicRadiator");
+	G4LogicalVolume *logicRadiator = new G4LogicalVolume(solidRadiator, Aerogel, "logicRadiator"); //creating the aergoel radiator from dimensions
 
-	G4VPhysicalVolume *physRadiator = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.25*m), logicRadiator, "physRadiator", logicWorld, false, 0, true);
+	G4VPhysicalVolume *physRadiator = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.25*m), logicRadiator, "physRadiator", logicWorld, false, 0, true); //placing radiator into world volume
 
-	G4Box *solidDetector = new G4Box("solidDetector", 0.005*m, 0.005*m, 0.01*m);
+	G4Box *solidDetector = new G4Box("solidDetector", 0.005*m, 0.005*m, 0.01*m);  //setting the dimensions for the detectors
 
-	logicDetector = new G4LogicalVolume(solidDetector, worldMat, "logicDetector");
+	logicDetector = new G4LogicalVolume(solidDetector, worldMat, "logicDetector"); //creating the detectors, with same material as world
 
 	
 
@@ -66,7 +67,7 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
 		for(G4int j = 0; j < 100; j++)
 		{
 			
-			G4VPhysicalVolume *physDetector= new G4PVPlacement(0, G4ThreeVector(-0.5*m+(i+0.5)*m/100, -0.5*m+(j+0.5)*m/100, 0.49*m), logicDetector, "physDetector", logicWorld, false, j+i*100, true);
+			G4VPhysicalVolume *physDetector= new G4PVPlacement(0, G4ThreeVector(-0.5*m+(i+0.5)*m/100, -0.5*m+(j+0.5)*m/100, 0.49*m), logicDetector, "physDetector", logicWorld, false, j+i*100, true); //placing 100*100 detectors into the world
 
 		}
 	}
@@ -74,14 +75,14 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
 
 
 
-	return physWorld;
+	return physWorld; //returning the world, including everything inside
 }
 
 void MyDetectorConstruction::ConstructSDandField()
 {
-    MySensitiveDetector *sensDet = new MySensitiveDetector("SensitiveDetector");
+    MySensitiveDetector *sensDet = new MySensitiveDetector("SensitiveDetector"); //creating a sensitive detector class
 
     if(logicDetector != NULL)
-        logicDetector->SetSensitiveDetector(sensDet);
+        logicDetector->SetSensitiveDetector(sensDet); //assigning the detectors as senstive detectors (it generates a hit when a track traverses its volume)
 }
 	
